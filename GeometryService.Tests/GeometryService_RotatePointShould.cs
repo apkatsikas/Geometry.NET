c471692a7c4c59fe68145804768f6280f0edf515
apkatsikas <apkatsikas@gmail.com>
2024-09-08 17:28:40 -0400

    Add paramaterization

--- GeometryService.Tests/GeometryService_RotatePointShould.cs
+++ GeometryService.Tests/GeometryService_RotatePointShould.cs
@@ -6,19 +6,21 @@ namespace Geometry.UnitTests.Services
 {
     public class GeometryService_RotatePointShould
     {
-        [Fact]
-        public void RotatePoint_ShouldWork()
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void RotatePoint_ShouldWork(
+            Vector2 point, Vector2 center, double rotation, Vector2 expectedPoint)
         {
-            var point = new Vector2(5,-1);
-            var center =  new Vector2(0,0);
-            var rotation = -90;
-            var expectedPoint = new Vector2(-1, -5);
-
-
             var geometryService = new GeometryService();
             var rotationRadians = geometryService.DegreesToRadians(rotation);
             var rotatedPoint = geometryService.RotatePoint(point, center, rotationRadians);
             Assert.Equal(expectedPoint, rotatedPoint);
         }
+
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                new object[] { new Vector2(5,-1), new Vector2(0,0), -90, new Vector2(-1, -5) },
+            };
     }
 }
